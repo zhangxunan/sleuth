d99fd3debe001e5378d1ea95ed926f02648591be
章旭楠 <zhang_xn1314@163.com>
2018-02-09 11:13:29 +0800

    Initial commit

---
+++ user-service/src/main/java/com/mysoft/userservice/web/UserController.java
@@ -0,0 +1,16 @@
+package com.mysoft.userservice.web;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/hi")
+    public String hi() {
+        return "I'm forezp";
+    }
+}
